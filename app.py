1b66d5e40887e69917db2d3a74f14bc6194b501e
Ivan Plotnikov <home@Ivans-MacBook-Pro.local>
2023-12-11 18:16:24 +0300

    wip

--- app.py
+++ app.py
@@ -215,11 +215,11 @@ app.layout = dbc.Container([
         style_cell={'maxWidth': 300, 'textAlign': 'center', 'overflow': 'hidden', 'textOverflow': 'ellipsis', 'color': 'white', 'backgroundColor': '#000'},
     ),
     
-    # dcc.Graph(
-    #     id='heatmap',
-    #     figure=heatmap_fig,
-    #     style={'border': '2px solid black', "margin-bottom":"50px"}
-    # ),
+    dcc.Graph(
+        id='heatmap',
+        figure=heatmap_fig,
+        style={'border': '2px solid black', "margin-bottom":"50px"}
+    ),
     
     dcc.Markdown('''
         ### Target - Price
@@ -231,11 +231,11 @@ app.layout = dbc.Container([
     ''', style={'margin-left': '50px','fontSize': '18px'}),
     
     
-    # dcc.Graph(
-    #     id='price_fig',
-    #     figure=price_fig,
-    #     style={'border': '2px solid black', "margin-bottom":"20px"}
-    # ),
+    dcc.Graph(
+        id='price_fig',
+        figure=price_fig,
+        style={'border': '2px solid black', "margin-bottom":"20px"}
+    ),
     
     
     html.Div([
@@ -269,71 +269,71 @@ app.layout = dbc.Container([
     
     
     
-    # generate_hypothesis_block("Biserial", "sourse", "price_usd", 0.01143, 0.00056, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_source',
-    #     figure=histogram_sourse,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ),
+    generate_hypothesis_block("Biserial", "sourse", "price_usd", 0.01143, 0.00056, 0.05, True),
+    dcc.Graph(
+        id='histogram_source',
+        figure=histogram_sourse,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ),
     
-    # generate_hypothesis_block("Biserial", "host_response_time", "price_usd", 0.00564, 0.08907, 0.05, False),
-    # dcc.Graph(
-    #     id='histogram_host_response_time',
-    #     figure=histogram_host_response_time,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ),
+    generate_hypothesis_block("Biserial", "host_response_time", "price_usd", 0.00564, 0.08907, 0.05, False),
+    dcc.Graph(
+        id='histogram_host_response_time',
+        figure=histogram_host_response_time,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ),
     
-    # generate_hypothesis_block("Pearson", "host_response_rate", "price_usd", -0.02782, 0.0, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_response_rate',
-    #     figure=histogram_host_response_rate,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ), 
+    generate_hypothesis_block("Pearson", "host_response_rate", "price_usd", -0.02782, 0.0, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_response_rate',
+        figure=histogram_host_response_rate,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ), 
     
-    # generate_hypothesis_block("Pearson", "host_acceptance_rate", "price_usd", 0.05246, 0.0, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_acceptance_rate',
-    #     figure=histogram_host_acceptance_rate,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ), 
+    generate_hypothesis_block("Pearson", "host_acceptance_rate", "price_usd", 0.05246, 0.0, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_acceptance_rate',
+        figure=histogram_host_acceptance_rate,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ), 
     
-    # generate_hypothesis_block("Biserial", "host_is_superhost", "price_usd", 0.01452, 1e-05, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_is_superhost',
-    #     figure=histogram_host_is_superhost,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ), 
+    generate_hypothesis_block("Biserial", "host_is_superhost", "price_usd", 0.01452, 1e-05, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_is_superhost',
+        figure=histogram_host_is_superhost,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ), 
 
-    # generate_hypothesis_block("Pearson", "host_listings_count", "price_usd", 0.11413, 0.0, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_listings_count',
-    #     figure=histogram_host_listings_count,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ),
-    # generate_hypothesis_block("Pearson", "host_total_listings_count", "price_usd", 0.09509, 0.0, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_total_listings_count',
-    #     figure=histogram_host_total_listings_count,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ), 
-    # generate_hypothesis_block("Biserial", "host_verifications", "price_usd", 0.03171, 0.00056, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_host_verifications',
-    #     figure=histogram_host_verifications,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ), 
-    # generate_hypothesis_block("Pearson", "accommodates", "price_usd", 0.35318, 0.00056, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_accommodates',
-    #     figure=histogram_accommodates,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ),
-    # generate_hypothesis_block("Pearson", "number_of_reviews", "price_usd", -0.04753, 0.00056, 0.05, True),
-    # dcc.Graph(
-    #     id='histogram_number_of_reviews',
-    #     figure=histogram_number_of_reviews,
-    #     style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
-    # ),
+    generate_hypothesis_block("Pearson", "host_listings_count", "price_usd", 0.11413, 0.0, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_listings_count',
+        figure=histogram_host_listings_count,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ),
+    generate_hypothesis_block("Pearson", "host_total_listings_count", "price_usd", 0.09509, 0.0, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_total_listings_count',
+        figure=histogram_host_total_listings_count,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ), 
+    generate_hypothesis_block("Biserial", "host_verifications", "price_usd", 0.03171, 0.00056, 0.05, True),
+    dcc.Graph(
+        id='histogram_host_verifications',
+        figure=histogram_host_verifications,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ), 
+    generate_hypothesis_block("Pearson", "accommodates", "price_usd", 0.35318, 0.00056, 0.05, True),
+    dcc.Graph(
+        id='histogram_accommodates',
+        figure=histogram_accommodates,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ),
+    generate_hypothesis_block("Pearson", "number_of_reviews", "price_usd", -0.04753, 0.00056, 0.05, True),
+    dcc.Graph(
+        id='histogram_number_of_reviews',
+        figure=histogram_number_of_reviews,
+        style={'display': 'inline-block', 'width': '60%', 'vertical-align': 'top', 'margin-top': '60px'}
+    ),
     generate_hypothesis_block("Pearson", "host_has_profile_pic", "price_usd", 0.01902, 0.00056, 0.05, True),
     dcc.Graph(
         id='histogram_host_has_profile_pic',
@@ -346,11 +346,11 @@ app.layout = dbc.Container([
     html.H3("Data Engineering", style={'margin': '60px'}),
     
     
-    # html.Div([
-    #     dcc.Markdown('''
-    #         Используем `latitude` и `longitude` для представления географических координат недвижимости в разных городах Италии.
-    #     '''),
-    # ], style={'fontSize': '18px', 'margin-left': '50px'}),
+    html.Div([
+        dcc.Markdown('''
+            Используем `latitude` и `longitude` для представления географических координат недвижимости в разных городах Италии.
+        '''),
+    ], style={'fontSize': '18px', 'margin-left': '50px'}),
     
     dcc.Graph(
         id='densitymapbox_price',
